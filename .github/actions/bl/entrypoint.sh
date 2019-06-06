c57699e1959124558a191ace3bc06c8bcedb38a9
Solomon Hykes <sh.github.6811@hykes.org>
2019-06-07 00:23:17 +0200

    Update entrypoint.sh

--- .github/actions/bl/entrypoint.sh
+++ .github/actions/bl/entrypoint.sh
@@ -4,4 +4,4 @@ set -ex
 
 find ./crate/code/web
 
-bl line run $BL_PIPELINE -o npm.build.web.src=@./crate/code/web/
+bl line run $BL_PIPELINE -o npm.build.web.src=@./crate/code/web/ -o fs.write_text.web_api_url.text=https://localhost:4242
