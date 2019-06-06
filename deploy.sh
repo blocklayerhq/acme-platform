a61177e9f1e83c368bc7e9a8f520766a5eeca977
Solomon Hykes <sh.github.6811@hykes.org>
2019-06-06 22:59:48 +0000

    deploy.sh: run pipelines on blocklayer platform instead of local runtime
    Signed-off-by: Solomon Hykes <sh.github.6811@hykes.org>

--- deploy.sh
+++ deploy.sh
@@ -1,48 +1,24 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-. deploy.env.secret
+if [ -z "$1" ]; then
+	echo >&2 Unspecified pipeline name
+	exit 1
+fi
 
-FLAGS=(
-	-o netlify.deploy.web.site-id=STRING:"$NETLIFY_SITE_ID"
-	-o netlify.deploy.web.auth-token=STRING:"$NETLIFY_AUTH_TOKEN"
-	-o fs.write_text.web_self_url.text=STRING:APP_URL="$WEB_URL"
-	-o fs.write_text.web_api_url.text=STRING:APP_URL_API="$API_URL"
-	-o gcr.push.backend.service-key=STRING:"$GCLOUD_SERVICE_KEY"
-	-o gcr.push.backend.project=STRING:"$GCLOUD_PROJECT"
-	-o kubernetes.kustomize.backend.src=FSTREE:./codeamp/backend-k8s
-	-o gke.deploy.backend.project=STRING:"$GCLOUD_PROJECT"
-	-o gke.deploy.backend.region=STRING:"$GCLOUD_COMPUTE_ZONE"
-	-o gke.deploy.backend.service-key=STRING:"$GCLOUD_SERVICE_KEY"
-	-o gke.deploy.backend.kubernetes-cluster=STRING:"$KUBERNETES_CLUSTER"
-	-o gke.deploy.backend.namespace=STRING:"$KUBERNETES_NAMESPACE"
-)
+. overrides.microstaging.secret
 
-if [ "$UPSTREAM" ]; then
+if [ "$LOCAL" ]; then
+	echo "---> Building from local repositories"
+	OVERRIDES+=(
+		-o npm.build.web.src=@./crate/code/web/
+	)
+else
 	echo "---> Building from upstream repositories"
-else
-	echo "---> Buildinfg from local repositories"
-	FLAGS+=(
-		-o npm.build.web.src=FSTREE:./crate/code/web/
-	)
 fi
 
-if [ "$DEV" ]; then
-	echo "---> Running pipeine with local development flags"
-	FLAGS+=(
-		--workdir /tmp/bl
-		--no-versioning
-	)
-fi
+pipeline="$1"
+shift
 
-blrun() {
-	bl-runtime run "${FLAGS[@]}" -f ./acme-clothing.pipeline "$@" 
-}
-
-if [ "$PRETTY" ]; then
-	echo "---> Prettifying pipeline output"
-	blrun "$@" 2>&1 | jq -r 'select(.level == "error" or .log_output == "stdout" or .log_output == "stderr" or .level == "fatal") | .level + " " + .bot_id + ": " + .message + .error'
-else
-	blrun "$@"
-fi
+bl line run -d "$pipeline" "${OVERRIDES[@]}" "$@"
