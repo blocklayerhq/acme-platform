1be3c0c410877e00a452b2e3521ae88f39f0c4b6
Solomon Hykes <sh.github.6811@hykes.org>
2019-06-07 16:46:03 +0200

    Remove debug override from staging github action

--- .github/actions/bl/entrypoint.sh
+++ .github/actions/bl/entrypoint.sh
@@ -4,4 +4,4 @@ set -ex
 
 find ./crate/code/web
 
-bl line run $BL_PIPELINE -o fs.write_text.web_node_env.src=@./crate/code/web/ -o fs.write_text.web_api_url.text=https://localhost:4242
+bl line run $BL_PIPELINE -o fs.write_text.web_node_env.src=@./crate/code/web/
