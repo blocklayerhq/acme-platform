8788e09f4270b1f7922cfe57f8ee756de9c465d4
Solomon Hykes <sh.github.6811@hykes.org>
2019-07-06 17:32:17 +0200

    V2 blueprint simplifies demo setup
    Signed-off-by: Solomon Hykes <sh.github.6811@hykes.org>

--- demo-setup-base.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-bl line rm acme-clothing-base || true
-bl line create acme-clothing-base -f ./acme-clothing.pipeline -d "Base Developer Pipeline for ACME"
-./blrun acme-clothing-base
--- demo-setup-staging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-bl line rm acme-clothing-staging || true
-bl line create acme-clothing-staging -f ./acme-clothing.pipeline -d "ACME Staging Pipeline"
-./blrun acme-clothing-staging
