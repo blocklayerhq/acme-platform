d5a7a55acae96d5c4889f49e2f829fffcb1229c9
Andrea Luzzardi <aluzzardi@gmail.com>
2020-03-06 17:59:36 -0800

    push.sh: "encrypt" token
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- push.sh
+++ push.sh
@@ -11,7 +11,7 @@ DRAFT="$DOMAIN-$RANDOM-$RANDOM"
 bl draft init "$DRAFT"
 
 read -s -r -p "Enter netlify API token: " NETLIFY_TOKEN
-bl push "$DOMAIN" --draft "$DRAFT" -k text -t staging.frontend.netlifyAccount.token.value "$NETLIFY_TOKEN"
+bl push "$DOMAIN" --draft "$DRAFT" -k text -t staging.frontend.netlifyAccount.token.value "$(echo -n "$NETLIFY_TOKEN" | base64)"
 
 bl push "$DOMAIN" --draft "$DRAFT" .
 bl push "$DOMAIN" --draft "$DRAFT" -t staging.monorepo "$MONOREPO"
